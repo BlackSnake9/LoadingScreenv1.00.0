111f905837fb06dda6a5f65c580f2d87748ac222
Logan <49781823+DriFtRagnarok@users.noreply.github.com>
2019-06-02 21:10:10 -0500

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,17 @@
+---- // -------------------------------\\ ----
+--- // ---- Made by DriFt Ragnarok ---- \\ ---
+-- // ---------------------------------- \\ --
+
+
+files{
+    '/resources/index.html',
+    '/resources/style.css',
+    '/resources/lossantos.jpg',
+    '/resources/anim_stripe.gif',
+    '/resources/logo.png',
+    '/resources/Loading.ogg',
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
